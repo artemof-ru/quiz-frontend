4e67f5fbeb5802d6bd8cee41ab44a37219a4f55f
Aleksandr <artemof.ru@yandex.ru>
2025-04-12 22:10:48 +0600

    change 3 question

--- src — old/components/Start.jsx
+++
@@ -1,8 +0,0 @@
-export default function Start () {
-	return (
-		<div className="start-page">
-			<h1>Добро пожаловать!</h1>
-			<p>Скоро будет доступен <br /> первый вопрос.</p>
-		</div>
-	)
-}
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const { Server } = require('socket.io');
+
 import React, {useRef, useState, useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
